5c2371c3d84465617116e65a88e0c4208f19f5fa
azhoubsj <138343605+azhoubsj@users.noreply.github.com>
2023-09-13 11:31:57 +0800

    add filtering function

--- read.py
+++ read.py
@@ -1,6 +1,7 @@
 data = []
 count = 0
 length = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
@@ -9,5 +10,15 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
+
+
 print('档案读取完了，总共有', len(data), '笔资料')
 print('每笔资料平均长度为', length / count)
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('长度小于100的资料总共有', len(new), '笔')
+print(new[0])
+print(new[1])
