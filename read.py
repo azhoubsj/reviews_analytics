4ba147a2c1e3b74e49be220e03ca694c5ea6a04d
azhoubsj <138343605+azhoubsj@users.noreply.github.com>
2023-09-13 11:14:42 +0800

    Update read.py

--- read.py
+++ read.py
@@ -1,12 +1,13 @@
 data = []
 count = 0
+length = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
+		length += len(data[count]) #用 length += len(line) 也可以
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
 
-print(data[0])
-print('-----------------------------')
-print(data[1])
+print('档案读取完了，总共有', len(data), '笔资料')
+print('每笔资料平均长度为', length / count)
