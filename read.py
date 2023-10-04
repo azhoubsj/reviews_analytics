b277f1651b0c57d05250cd4c88d14ef3e8694b41
azhoubsj <138343605+azhoubsj@users.noreply.github.com>
2023-10-04 19:24:05 +0800

    add word count function

--- read.py
+++ read.py
@@ -5,14 +5,15 @@ length = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
-		length += len(data[count]) #用 length += len(line) 也可以
+		# length += len(data[count]) #用 length += len(line) 也可以
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
 
 
-
 print('档案读取完了，总共有', len(data), '笔资料')
+
+
 print('每笔资料平均长度为', length / count)
 
 new = []
@@ -28,4 +29,32 @@ for d in data:
 	if 'good' in d:
 		good.append(d) #这四行 等于 good = [d for d in data if 'good' in d]
 print('含有good的资料总共有', len(good), '笔')
-print(good[0])
+
+
+# 文字注解
+
+wc = {} # word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 # 新增新的key进wc字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+print(len(wc))
+
+while True:
+	word = input('请问你想要查询什么字：')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出现过的次数为： ', wc[word])
+	else:
+		print('这个字没有出现过哦')
+
+print('感谢使用本查询功能')
+
